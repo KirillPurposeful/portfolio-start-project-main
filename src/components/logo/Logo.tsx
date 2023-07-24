54e86890fc5c4e7118499fb7acc7d8e2fa6974fd
KirillPurposeful <kirill.qa@megapartners.io>
2023-07-25 01:43:34 +0200

    add file structure and header

---
+++ src/components/logo/Logo.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import {Icon} from "../icon/Icon";
+
+export const Logo = () => {
+    return (
+            <a href="https://www.shrekbucks.com/portfolio">
+                <Icon iconSrc={'react'}/>
+            </a>
+    );
+};
+
