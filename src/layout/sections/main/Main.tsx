ee7ce09cfb78774859ecf4ac6c0e12211e8fe258
KirillPurposeful <kirill.qa@megapartners.io>
2023-07-25 10:48:19 +0200

    add file structure and sections/main

---
+++ src/layout/sections/main/Main.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import styled from "styled-components";
+import shrekPhoto from '../../../assets/images/shrek.webp'
+import {FlexWrapper} from "../../../components/FlexWrapper";
+export const Main = () => {
+    return (
+
+            <StyledMain>
+                <FlexWrapper align={"center"} justify={"space-around"}>
+                    <div>
+                <span>Hi folks!</span>
+                    <h2>I am SHREK</h2>
+                <h1>Software engineer</h1>
+                    </div>
+                <Photo src={shrekPhoto} alt=""/>
+                </FlexWrapper>
+            </StyledMain>
+
+
+    );
+};
+
+const StyledMain = styled.div`
+  min-height: 10vh;
+  background-color: #0b360b;
+  color: white;
+`
+
+const Photo = styled.img`
+    width: 430px;
+    height: 500px;
+    object-fit: cover;
+ `
